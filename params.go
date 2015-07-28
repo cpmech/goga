b839f179835dc7d4bbef00662f8cb893a4ba9f27
Dorival Pedroso <pedroso@cpmech.com>
2015-07-28 16:30:39 +1000

    use float random added to params

--- params.go
+++ params.go
@@ -13,19 +13,21 @@ import (
 
 // Params is an auxiliary structure to hold input parameters for setting the GA up
 type Params struct {
-	Nislands int     // number of islands
-	Ninds    int     // number of individuals: population size
-	Nbases   int     // number of bases in chromosome
-	Tf       int     // number of generations
-	Dtout    int     // increment of time for output
-	Dtmig    int     // increment of time for migration
-	Pc       float64 // probability of crossover
-	Pm       float64 // probability of mutation
-	Elite    bool    // use elitism
-	Rws      bool    // use Roulette-Wheel selection method
-	Rnk      bool    // ranking
-	RnkSP    float64 // selective pressure for ranking
-	Fnkey    string  // key for reports' filenames
+	Nislands  int     // number of islands
+	Ninds     int     // number of individuals: population size
+	Nbases    int     // number of bases in chromosome
+	Tf        int     // number of generations
+	Dtout     int     // increment of time for output
+	Dtmig     int     // increment of time for migration
+	Pc        float64 // probability of crossover
+	Pm        float64 // probability of mutation
+	Elite     bool    // use elitism
+	Rws       bool    // use Roulette-Wheel selection method
+	Rnk       bool    // ranking
+	RnkSP     float64 // selective pressure for ranking
+	Fnkey     string  // key for reports' filenames
+	UseIntRnd bool    // generate random integers instead of selecting from grid
+	UseFltRnd bool    // generate random float point numbers instead of selecting from grid
 }
 
 // ReadParams reads parameters from JSON file
@@ -48,6 +50,8 @@ func ReadParams(filenamepath string) *Params {
 	o.Rnk = true
 	o.RnkSP = 1.2
 	o.Fnkey = ""
+	o.UseIntRnd = false
+	o.UseFltRnd = false
 
 	// read file
 	b, err := io.ReadFile(filenamepath)
