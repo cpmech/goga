dc015cb2fca76fd5ef21f1c5ff8d19f2724f1b9f
Dorival Pedroso <pedroso@cpmech.com>
2015-08-02 12:06:26 +1000

    example on reliability analysis (beam) improved

--- examples/rel-simple-beam-form.go
+++ examples/rel-simple-beam-form.go
@@ -1,6 +1,8 @@
-// Copyright 2015 Dorival de Moraes Pedroso. All rights reserved.
+// Copyright 2012 Dorival de Moraes Pedroso. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+// +build ignore
 
 package main
 
@@ -11,81 +13,54 @@ import (
 	"github.com/cpmech/gosl/rnd"
 )
 
-// deflection computes the max deflection at mid-span of simply supported beam
-// with uniform distributed load q and concentrated load at midspan
-//  Input:
-//   L  -- span
-//   EI -- Young's modulus times cross-sectional moment of inertia
-//   p  -- concentrated load at mid-span
-//   q  -- distributed load
-//  Output:
-//   max deflection (vertical displacement) at mid-span
-func deflection(L, EI, p, q float64) float64 {
-	L3 := math.Pow(L, 3.0)
-	return p*L3/EI/48.0 + 5.0*q*L3*L/EI/384.0
-}
-func deflection_derivs(L, EI, p, q float64) (dp, dq float64) {
-	L3 := math.Pow(L, 3.0)
-	dp = L3 / EI / 48.0
-	dq = 5.0 * L3 * L / EI / 384.0
-	return
-}
-
 func main() {
 
-	// Example from
-	// Achintya Haldar, Reliability-Based Structura Design, 2005
+	// Simply supported beam
+	// Analyse the max deflection at mid-span of simply supported beam
+	// with uniform distributed load q and concentrated load at midspan
+	//  Data:
+	//   L    -- span
+	//   EI   -- Young's modulus times cross-sectional moment of inertia
+	//   p    -- x[0] concentrated load at mid-span
+	//   q    -- x[1] distributed load
+	//   δlim -- max deflection (vertical displacement) at mid-span
+	// Reference
+	//  Haldar A, Reliability-Based Structura Design, 2005
 
+	// constants
 	δlim := 0.0381 // [m] max allowed deflection
-	L := 9.144     // [m]
-	EI := 182262.0 // [kN m²]
-	μp := 111.2    // [kN] mean of p (lognormal random variable)
-	σp := 11.12    // [kN] deviation of p
-	μq := 35.03    // [kN/m] mean value of q (normal random variable)
-	σq := 5.25     // [kN/m] standard deviation of q
+	L := 9.144     // [m] span
+	EI := 182262.0 // [kN m²] flexural rigidity
+	L3 := math.Pow(L, 3.0)
 
-	_ = L
-	_ = EI
-	_ = σq
+	// statistics of p=x[0] and q=x[1]
+	μ := []float64{111.2, 35.03} // mean values
+	σ := []float64{11.12, 5.25}  // deviation values
+	lrv := []bool{true, false}   // is lognormal random variable?
 
-	// limit state function and derivative
-	gfcn := func(p, q float64) float64 {
-		return δlim - deflection(L, EI, p, q)
-	}
-	dgfcn := func(p, q float64) (dgdp, dgdq float64) {
-		dp, dq := deflection_derivs(L, EI, p, q)
-		dgdp, dgdq = -dp, -dq
+	// limit state function
+	gfcn := func(x []float64, args ...interface{}) (g float64, err error) {
+		p, q := x[0], x[1]
+		g = δlim - (p*L3/EI/48.0 + 5.0*q*L3*L/EI/384.0)
 		return
 	}
 
-	_ = gfcn
+	// derivative of limit state function
+	hfcn := func(dgdx, x []float64, args ...interface{}) (err error) {
+		dgdx[0] = -L3 / EI / 48.0            // dg/dp
+		dgdx[1] = -5.0 * L3 * L / EI / 384.0 // dg/dq
+		return
+	}
 
-	var ln rnd.LogNormal
-	ln.Sig = σp / μp
-	ln.Mu = math.Log(μp) - ln.Sig*ln.Sig/2.0 // TODO: check this equation
-	ln.CalcDerived()
+	// first order reliability method
+	var form rnd.ReliabFORM
+	form.Init(μ, σ, lrv, gfcn, hfcn)
+	form.TolA = 0.005
+	form.TolB = 0.005
 
-	β := 3.0
-	ps := μp // p-star: trial intersection point
-	qs := μq // q-star: trial intersection point
-
-	ps = 123.841
-
-	fp := ln.Pdf(ps)
-	Φinvp := (math.Log(ps) - ln.Mu) / ln.Sig
-	Φinvp = 1.13
-	φp := math.Exp(-Φinvp*Φinvp/2.0) / math.Sqrt2 / math.SqrtPi
-	σpN := φp / fp
-	μpN := ps - Φinvp*σpN
-
-	dp, dq := dgfcn(ps, qs)
-	den := math.Sqrt(math.Pow(dp*σpN, 2.0) + math.Pow(dq*σq, 2.0))
-	αp := dp * σpN / den
-	αq := dq * σq / den
-
-	io.Pfpink("αp = %v\n", αp)
-	io.Pfpink("αq = %v\n", αq)
-
-	_ = β
-	_ = μpN
+	// run FORM
+	verbose := false // show messages
+	βtrial := 3.0
+	β, _, _, _ := form.Run(βtrial, verbose)
+	io.Pforan("β(final) = %v\n", β)
 }
