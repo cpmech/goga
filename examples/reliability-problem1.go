a98dfd03f367ab96c8884731b0a9709de0741392
Dorival Pedroso <pedroso@cpmech.com>
2015-08-04 09:56:00 +1000

    example added and is working

---
+++ examples/reliability-problem1.go
@@ -0,0 +1,116 @@
+// Copyright 2012 Dorival de Moraes Pedroso. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	"bytes"
+	"math"
+
+	"github.com/cpmech/goga"
+	"github.com/cpmech/gosl/io"
+	"github.com/cpmech/gosl/la"
+	"github.com/cpmech/gosl/rnd"
+	"github.com/cpmech/gosl/utl"
+)
+
+func calc_beta(best *goga.Individual, βref float64, verbose bool) (β float64) {
+	xs := make([]float64, best.Nfltgenes)
+	for i := 0; i < best.Nfltgenes; i++ {
+		xs[i] = best.GetFloat(i)
+	}
+	β = math.Sqrt(la.VecDot(xs, xs))
+	if verbose {
+		io.Pf("\nova = %g  oor = %g\n", best.Ova, best.Oor)
+		io.Pf("x   = %v\n", xs)
+		io.PfYel("β   = %g", β)
+		io.Pf(" (%g)\n", βref)
+	}
+	return
+}
+
+func main() {
+
+	rnd.Init(0)
+
+	// standard variables => μ=0 and σ=1 => y = x
+
+	g := func(x []float64) float64 {
+		return 0.1*math.Pow(x[0]-x[1], 2.0) - (x[0]+x[1])/math.Sqrt2 + 2.5
+	}
+
+	x := make([]float64, 2)
+	ovfunc := func(ind *goga.Individual, idIsland, time int, report *bytes.Buffer) (ova, oor float64) {
+		x[0], x[1] = ind.GetFloat(0), ind.GetFloat(1)
+		fp := utl.GtePenalty(1e-2, math.Abs(g(x)), 1)
+		ova = la.VecDot(x, x) + fp
+		oor = fp
+		return
+	}
+
+	// parameters
+	C := goga.NewConfParams()
+	C.Pll = false
+	C.Nisl = 4
+	C.Ninds = 20
+	C.DoPlot = true
+	C.CalcDerived()
+
+	// bingo
+	ndim := 2
+	vmin, vmax := -1.0, 1.0
+	xmin, xmax := utl.DblVals(ndim, vmin), utl.DblVals(ndim, vmax)
+	bingo := goga.NewBingoFloats(xmin, xmax)
+
+	// evolver
+	βref := 2.5
+	evo := goga.NewEvolverFloatChromo(C, xmin, xmax, ovfunc, bingo)
+
+	// for a number of trials
+	ntrials := 100
+	betas := make([]float64, ntrials)
+	for i := 0; i < ntrials; i++ {
+
+		// reset population
+		if i > 0 {
+			for _, isl := range evo.Islands {
+				isl.Pop.GenFloatRandom(C, xmin, xmax)
+			}
+		}
+		pop0 := evo.Islands[0].Pop
+
+		// run
+		check := i == ntrials-1
+		verbose := check
+		doreport := check
+		evo.Run(verbose, doreport)
+		β := calc_beta(evo.Best, βref, verbose)
+		betas[i] = β
+
+		// plot contour
+		if check {
+			if C.DoPlot {
+				xmin := []float64{-1, -1}
+				xmax := []float64{5, 5}
+				goga.PlotTwoVarsContour("/tmp/goga", "reliability-problem1", pop0, evo.Islands[0].Pop, evo.Best,
+					xmin, xmax, 41, true, nil, g, g)
+			}
+		}
+	}
+
+	// analysis
+	βmin, βave, βmax, βdev := rnd.StatBasic(betas, true)
+	io.Pf("\nβmin = %v\n", βmin)
+	io.PfYel("βave = %v\n", βave)
+	io.Pf("βmax = %v\n", βmax)
+	io.Pf("βdev = %v\n\n", βdev)
+	io.Pf(rnd.BuildTextHist(nice_num(βmin-0.005), nice_num(βmax+0.005), 11, betas, "%.3f", 60))
+}
+
+func nice_num(x float64) float64 {
+	s := io.Sf("%.2f", x)
+	return io.Atof(s)
+}
