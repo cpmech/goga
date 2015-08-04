9ee9510467bae3f5dc41b823d826543a9bb2edea
Dorival Pedroso <pedroso@cpmech.com>
2015-08-04 12:09:11 +1000

    problems added

--- examples/rel-prob1.go
+++ examples/rel-prob1.go
@@ -20,6 +20,18 @@ import (
 
 func main() {
 
+	// Problems where all variables are standard variables => μ=0 and σ=1 => y = x
+	// References
+	//  [1] Santos SR, Matioli LC and Beck AT. New optimization algorithms for structural
+	//      reliability analysis. Computer Modeling in Engineering & Sciences, 83(1):23-56; 2012
+	//      doi:10.3970/cmes.2012.083.023
+	//  [2] Borri A and Speranzini E. Structural reliability analysis using a standard deterministic
+	//      finite element code. Structural Safety, 19(4):361-382; 1997
+	//      doi:10.1016/S0167-4730(97)00017-9
+	//  [3] Grooteman F.  Adaptive radial-based importance sampling method or structural
+	//      reliability. Structural safety, 30:533-542; 2008
+	//      doi:10.1016/j.strusafe.2007.10.002
+
 	// catch errors
 	defer func() {
 		if err := recover(); err != nil {
@@ -27,26 +39,37 @@ func main() {
 		}
 	}()
 
-	// Problem # 1:
-	//  All variables are standard variables => μ=0 and σ=1 => y = x
-
 	// read parameters
 	fn := "rel-prob1"
 	fn, fnkey := io.ArgToFilename(0, fn, ".json", true)
 	C := goga.ReadConfParams(fn)
+	if C.ProbId < 1 || C.ProbId > 2 {
+		C.ProbId = 1
+	}
 
 	// initialise random numbers generator
 	rnd.Init(C.Seed)
 
 	// limit state function
-	g := func(x []float64) float64 {
-		return 0.1*math.Pow(x[0]-x[1], 2.0) - (x[0]+x[1])/math.Sqrt2 + 2.5
+	var g func(x []float64) float64
+	switch C.ProbId {
+	case 1: // problem # 1 from [1,2]
+		g = func(x []float64) float64 {
+			return 0.1*math.Pow(x[0]-x[1], 2.0) - (x[0]+x[1])/math.Sqrt2 + 2.5
+		}
+	case 2: // problem # 2 from [1,2]
+		g = func(x []float64) float64 {
+			return -0.5*math.Pow(x[0]-x[1], 2.0) - (x[0]+x[1])/math.Sqrt2 + 3.0
+		}
+	case 3: // problem # 3 from [1] and # 6 from [3]
+		g = func(x []float64) float64 {
+			return 2.0 - x[1] - 0.1*math.Pow(x[0], 2) + 0.06*math.Pow(x[0], 3)
+		}
 	}
 
 	// objective value function
-	x := make([]float64, 2)
-	ovfunc := func(ind *goga.Individual, idIsland, time int, report *bytes.Buffer) (ova, oor float64) {
-		x[0], x[1] = ind.GetFloat(0), ind.GetFloat(1)
+	ovfunc := func(ind *goga.Individual, idIsland, t int, report *bytes.Buffer) (ova, oor float64) {
+		x := []float64{ind.GetFloat(0), ind.GetFloat(1)} // must be inside ovfunc to avoid data race problems
 		fp := utl.GtePenalty(1e-2, math.Abs(g(x)), 1)
 		ova = la.VecDot(x, x) + fp
 		oor = fp
