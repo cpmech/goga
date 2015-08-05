4e74400a9194e44154055ae7336da1435bce619f
Dorival Pedroso <pedroso@cpmech.com>
2015-08-05 22:12:32 +1000

    reliability example now uses random variables from package rnd. code cleaned quite a bit. strategy # 3 and 4 removed because inverse mapping doesn't work

--- examples/monitor.bash
+++ examples/monitor.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #FILE="rel-simple-beam-form.go"
-FILE="rel-prob1to5.go"
+FILE="rel-2d-simple.go"
 
 while true; do
     inotifywait -q -e modify $FILE
--- examples/rel-2d-simple.go
+++ examples/rel-2d-simple.go
@@ -15,7 +15,6 @@ import (
 	"github.com/cpmech/gosl/chk"
 	"github.com/cpmech/gosl/io"
 	"github.com/cpmech/gosl/la"
-	"github.com/cpmech/gosl/num"
 	"github.com/cpmech/gosl/plt"
 	"github.com/cpmech/gosl/rnd"
 	"github.com/cpmech/gosl/utl"
@@ -73,15 +72,11 @@ func main() {
 	rnd.Init(C.Seed)
 
 	// problems's data
-	ϵ := 1e-2                       // tolerance for strategy 2
+	ϵ := 0.1                        // tolerance for strategy 2
 	npts := 101                     // number of points for contour plot
 	var g func(x []float64) float64 // limit state function
 	var βref float64                // reference β (if available)
-	var xref []float64              // reference x (if available)
-	var μ, σ []float64              // mean and std deviation (if not standard)
-	var ds []string                 // distribution. <nil> means standard with μ=0, σ=1
-	vmin := []float64{-4, -4}       // x or y min. default is standard vars; thus x==y
-	vmax := []float64{4, 4}         // x or y max. default is standard vars; thus x==y
+	var vars rnd.Variables          // random variables data
 	axequal := true                 // plot with axis.equal
 
 	// set problem
@@ -92,27 +87,33 @@ func main() {
 		g = func(x []float64) float64 {
 			return 0.1*math.Pow(x[0]-x[1], 2) - (x[0]+x[1])/math.Sqrt2 + 2.5
 		}
-		βref = 2.5 // from [1]
-		xref = []float64{1.7677, 1.7677}
-		μ = []float64{0, 0}
-		σ = []float64{1, 1}
-		ds = []string{"nrm", "nrm"}
+		βref = 2.5 // from [1] xref = []float64{1.7677, 1.7677}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 0, S: 1},
+			&rnd.VarData{D: rnd.D_Normal, M: 0, S: 1},
+		}
 
 	// problem # 2 of [1] and Eq. (A.6) of [2]
 	case 2:
 		g = func(x []float64) float64 {
 			return -0.5*math.Pow(x[0]-x[1], 2) - (x[0]+x[1])/math.Sqrt2 + 3
 		}
-		βref = 1.658 // from [2]
-		xref = []float64{-0.7583, 1.4752}
+		βref = 1.658 // from [2] xref = []float64{-0.7583, 1.4752}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 0, S: 1},
+			&rnd.VarData{D: rnd.D_Normal, M: 0, S: 1},
+		}
 
 	// problem # 3 from [1] and # 6 from [3]
 	case 3:
 		g = func(x []float64) float64 {
 			return 2 - x[1] - 0.1*math.Pow(x[0], 2) + 0.06*math.Pow(x[0], 3)
 		}
-		βref = 2 // from [1]
-		xref = []float64{0, 2}
+		βref = 2 // from [1] xref = []float64{0, 2}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 0, S: 1},
+			&rnd.VarData{D: rnd.D_Normal, M: 0, S: 1},
+		}
 
 	// problem # 4 from [1] and # 8 from [3]
 	case 4:
@@ -120,17 +121,23 @@ func main() {
 			return 3 - x[1] + 256*math.Pow(x[0], 4)
 		}
 		npts = 101
-		βref = 3 // from [1]
-		xref = []float64{0, 3}
+		βref = 3 // from [1] xref = []float64{0, 3}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 0, S: 1},
+			&rnd.VarData{D: rnd.D_Normal, M: 0, S: 1},
+		}
 
 	// problem # 5 from [1] and # 1 from [4] (modified)
 	case 5:
-		shift := 0.1
+		shift := 0.0 //0.1
 		g = func(x []float64) float64 {
 			return 1 + math.Pow(x[0]+x[1]+shift, 2)/4 - 4*math.Pow(x[0]-x[1]+shift, 2)
 		}
-		βref = 0.3536 // from [1]
-		xref = []float64{-βref * math.Sqrt2 / 2, βref * math.Sqrt2 / 2}
+		βref = 0.3536 // from [1] xref = []float64{-βref * math.Sqrt2 / 2, βref * math.Sqrt2 / 2}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 0, S: 1},
+			&rnd.VarData{D: rnd.D_Normal, M: 0, S: 1},
+		}
 
 	// problem # 7 from [1] and example # 1 (case1) from [5]
 	// x1 and x2 are normally distributed and statistically independent with
@@ -138,11 +145,11 @@ func main() {
 		g = func(x []float64) float64 {
 			return math.Pow(x[0], 3) + math.Pow(x[1], 3) - 18
 		}
-		ϵ = 0.1
 		βref = 2.2401 // from [1]
-		μ = []float64{10, 10}
-		σ = []float64{5, 5}
-		ds = []string{"nrm", "nrm"}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 10, S: 5},
+			&rnd.VarData{D: rnd.D_Normal, M: 10, S: 5},
+		}
 
 	// problem # 8 from [1] and example # 1 (case2) from [5]
 	// x1 and x2 are normally distributed and statistically independent with
@@ -150,11 +157,11 @@ func main() {
 		g = func(x []float64) float64 {
 			return math.Pow(x[0], 3) + math.Pow(x[1], 3) - 18
 		}
-		ϵ = 0.1
 		βref = 2.2260 // from [1]
-		μ = []float64{10, 9.9}
-		σ = []float64{5, 5}
-		ds = []string{"nrm", "nrm"}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 10, S: 5},
+			&rnd.VarData{D: rnd.D_Normal, M: 9.9, S: 5},
+		}
 
 	// problem # 9 from [1] and case 7 of [3]
 	// x1 and x2 are normally distributed and statistically independent with
@@ -163,9 +170,10 @@ func main() {
 			return 2.5 - 0.2357*(x[0]-x[1]) + 0.0046*math.Pow(x[0]+x[1]-20, 4)
 		}
 		βref = 2.5 // from [1]
-		μ = []float64{10, 10}
-		σ = []float64{3, 3}
-		ds = []string{"nrm", "nrm"}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 10, S: 3},
+			&rnd.VarData{D: rnd.D_Normal, M: 10, S: 3},
+		}
 
 	// problem # 10 from [1] and example # 2 from [5]
 	// x1 and x2 are normally distributed and statistically independent with
@@ -174,9 +182,10 @@ func main() {
 			return math.Pow(x[0], 3) + math.Pow(x[1], 3) - 67.5
 		}
 		βref = 1.9003 // from [1]
-		μ = []float64{10, 10}
-		σ = []float64{5, 5}
-		ds = []string{"nrm", "nrm"}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 10, S: 5},
+			&rnd.VarData{D: rnd.D_Normal, M: 10, S: 5},
+		}
 
 	// problem # 11 from [1] and case 2 of [3]
 	// x1 and x2 are normally distributed and statistically independent with
@@ -185,12 +194,11 @@ func main() {
 			return x[0]*x[1] - 146.14
 		}
 		βref = 5.4280 // from [1]
-		μ = []float64{78064.4, 0.0104}
-		σ = []float64{11709.7, 0.00156}
-		ds = []string{"nrm", "nrm"}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 78064.4, S: 11709.7},
+			&rnd.VarData{D: rnd.D_Normal, M: 0.0104, S: 0.00156},
+		}
 		axequal = false
-		vmin = []float64{-6, -6} // x or y min. default is standard vars; thus x==y
-		vmax = []float64{6, 6}   // x or y max. default is standard vars; thus x==y
 
 	// problem # 12 from [1] and example 2 of [4]
 	// x1 and x2 are normally distributed and statistically independent with
@@ -198,11 +206,11 @@ func main() {
 		g = func(x []float64) float64 {
 			return 2.2257 - 0.025*math.Sqrt2*math.Pow(x[0]+x[1]-20, 3)/27 + 0.2357*(x[0]-x[1])
 		}
-		//ϵ = 0.5
 		βref = 2.2257 // from [1]
-		μ = []float64{10, 10}
-		σ = []float64{3, 3}
-		ds = []string{"nrm", "nrm"}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Normal, M: 10, S: 3},
+			&rnd.VarData{D: rnd.D_Normal, M: 10, S: 3},
+		}
 
 	// problem # 14 from [1]
 	// x1 and x2 are log-normally distributed and statistically independent with
@@ -210,63 +218,39 @@ func main() {
 		g = func(x []float64) float64 {
 			return x[0]*x[1] - 1140
 		}
-		ϵ = 0.5
 		βref = 5.2127 // from [1] // from here: 5.210977819456551
-		μ = []float64{38, 54}
-		σ = []float64{3.8, 2.7}
-		ds = []string{"nrm", "nrm"}
-		ds = []string{"logMuSig", "logMuSig"}
-		vmin = []float64{0.01, 0.01}
-		vmax = []float64{20, 20}
+		vars = rnd.Variables{
+			&rnd.VarData{D: rnd.D_Log, M: 38, S: 3.8, Std: true},
+			&rnd.VarData{D: rnd.D_Log, M: 54, S: 2.7, Std: true},
+		}
 
 	default:
 		chk.Panic("problem number %d is invalid", C.Problem)
 	}
 
-	// check
-	//μN, σN := lognormal_calc_equiv_prms_from_musig(μ[0], σ[0], 38.0)
-	//io.Pforan("μN=%v σN=%v\n", μN, σN)
-	//return
-
-	// use original variables
-	if C.Strategy < 3 && μ != nil {
-		vmin, vmax = make([]float64, 2), make([]float64, 2)
-		for i := 0; i < 2; i++ {
-			vmin[i] = μ[i] - 1.3*μ[i]
-			vmax[i] = μ[i] + 1.3*μ[i]
-			if ds[i][:3] == "log" && vmin[i] < TOLMINLOG {
-				vmin[i] = TOLMINLOG
-			}
-		}
+	// initialise distributions
+	err := vars.Init()
+	if err != nil {
+		chk.Panic("cannot initialise distributions:\n%v", err)
 	}
 
-	//vmin = []float64{20, 42}
-	//vmax = []float64{30, 50}
-	//vmax = []float64{15, 15}
+	// guess search space
+	nvars := len(vars)
+	vmin, vmax := make([]float64, nvars), make([]float64, nvars)
+	for i, d := range vars {
+		vmin[i] = d.M - 4*d.S
+		vmax[i] = d.M + 4*d.S
+		if d.D == rnd.D_Log && vmin[i] < rnd.TOLMINLOG {
+			vmin[i] = rnd.TOLMINLOG
+		}
+	}
 
 	// objective value function
 	ovfunc := func(ind *goga.Individual, idIsland, t int, report *bytes.Buffer) (ova, oor float64) {
 
-		// get original and normalised variables
-		var x, y []float64
-		var invalid bool
-		switch C.Strategy {
-
-		// operates on x (original)
-		case 1, 2:
-			x = ind.GetFloats()
-			y, invalid = calc_norm_vars(ds, μ, σ, x)
-
-		// operates on y (normalised)
-		case 3, 4:
-			y = ind.GetFloats()
-			x, invalid = calc_orig_vars(ds, μ, σ, y)
-
-		default:
-			chk.Panic("strategy %d is invalid", C.Strategy)
-		}
-
-		// handle invalid variable; e.g. yi<0 where yi is lognormal
+		// original and normalised variables
+		x := ind.GetFloats()
+		y, invalid := vars.Transform(x)
 		if invalid {
 			oor = 1e+8
 			return
@@ -277,33 +261,13 @@ func main() {
 
 		// compute objective value
 		switch C.Strategy {
-
-		// argmin_{x,y}{ β(y(x)) | g(x) ≤ 0 }
-		case 1, 3:
+		case 1: // argmin_{x,y}{ β(y(x)) | g(x) ≤ 0 }
 			ova = b                          // ova ← y dot y
 			oor = utl.GtePenalty(0, g(x), 1) // oor ← 0 ≥ g(x)
-
-		// argmin_{x,y}{ β(y(x)) + c(x) | |g(x)| ≤ ϵ }
-		case 2, 4:
+		case 2: // argmin_{x,y}{ β(y(x)) + c(x) | |g(x)| ≤ ϵ }
 			c := utl.GtePenalty(ϵ, math.Abs(g(x)), 1) // c ← ϵ ≥ |g(x)|
 			ova = b + c                               // ova ← y dot y + c
 			oor = c                                   // oor ← c
-		}
-		return
-	}
-
-	// transformation functions (for plotting)
-	Tfcn := func(x []float64) (y []float64) {
-		y, invalid := calc_norm_vars(ds, μ, σ, x)
-		if invalid {
-			chk.Panic("Tfcn: invalid value")
-		}
-		return
-	}
-	Tifcn := func(y []float64) (x []float64) {
-		x, invalid := calc_orig_vars(ds, μ, σ, y)
-		if invalid {
-			chk.Panic("Tifcn: invalid value")
 		}
 		return
 	}
@@ -329,20 +293,27 @@ func main() {
 		// run
 		check := i == C.Ntrials-1
 		evo.Run(check, check)
-		β := calc_beta(evo.Best, βref, ds, μ, σ, xref, C.Strategy, check)
+
+		// compute β
+		x := evo.Best.GetFloats()
+		y, invalid := vars.Transform(x)
+		if invalid {
+			chk.Panic("best individual has invalid values")
+		}
+		β := math.Sqrt(la.VecDot(y, y))
 		betas[i] = β
+		if check {
+			io.Pf("β = %g (%g)\n\n", β, βref)
+		}
 
 		// plot contour
 		if check && C.DoPlot {
 			pop1 := evo.Islands[0].Pop
 			extra := func() { plt.SetXnticks(11); plt.SetYnticks(11) }
-			istrans := C.Strategy > 2
 			goga.PlotTwoVarsContour("/tmp/goga", io.Sf("rel-prob%d-ST%d-orig", C.Problem, C.Strategy), pop0, pop1, evo.Best, npts, extra, axequal,
-				vmin, vmax, istrans, false, Tfcn, Tifcn, g, g)
-			if len(ds) > 0 {
-				goga.PlotTwoVarsContour("/tmp/goga", io.Sf("rel-prob%d-ST%d-tran", C.Problem, C.Strategy), pop0, pop1, evo.Best, npts, extra, axequal,
-					vmin, vmax, istrans, true, Tfcn, Tifcn, g, g)
-			}
+				vmin, vmax, false, false, nil, nil, g, g)
+			goga.PlotTwoVarsContour("/tmp/goga", io.Sf("rel-prob%d-ST%d-tran", C.Problem, C.Strategy), pop0, pop1, evo.Best, npts, extra, axequal,
+				vmin, vmax, false, true, vars.Transform, nil, g, g)
 		}
 	}
 
@@ -350,119 +321,14 @@ func main() {
 	io.Pfcyan("\nelapsed time = %v\n", time.Now().Sub(cpu0))
 
 	// analysis
-	βmin, βave, βmax, βdev := rnd.StatBasic(betas, true)
-	io.Pf("\nβmin = %v\n", βmin)
-	io.PfYel("βave = %v\n", βave)
-	io.Pf("βmax = %v\n", βmax)
-	io.Pf("βdev = %v\n\n", βdev)
-	io.Pf(rnd.BuildTextHist(nice_num(βmin-0.005), nice_num(βmax+0.005), 11, betas, "%.3f", 60))
-}
-
-func lognormal_calc_equiv_prms(μ, σ, x float64, μσ_are_ms bool) (μN, σN float64) {
-	if x < TOLMINLOG {
-		chk.Panic("cannot compute μN and σN because x<0. x=%g", x)
+	if C.Ntrials > 1 {
+		βmin, βave, βmax, βdev := rnd.StatBasic(betas, true)
+		io.Pf("\nβmin = %v\n", βmin)
+		io.PfYel("βave = %v\n", βave)
+		io.Pf("βmax = %v\n", βmax)
+		io.Pf("βdev = %v\n\n", βdev)
+		io.Pf(rnd.BuildTextHist(nice_num(βmin-0.005), nice_num(βmax+0.005), 11, betas, "%.3f", 60))
 	}
-	m, s := μ, σ
-	if !μσ_are_ms { // compute lognormal variables from statistics of x
-		δ := σ / μ
-		s = math.Sqrt(math.Log(1 + δ*δ))
-		m = math.Log(μ) - s*s/2
-	}
-	σN = s * x
-	μN = (1 - math.Log(x) + m) * x
-	//io.Pfpink("μ=%v σ=%v x=%v\n", μ, σ, x)
-	//io.Pfpink("δ=%v s=%v m=%v\n", δ, s, m)
-	//io.Pfpink("μN=%v σN=%v\n", μN, σN)
-	return
-}
-
-// calc_norm_vars computes normal variables from original variables
-func calc_norm_vars(ds []string, μ, σ, x []float64) (y []float64, invalid bool) {
-	y = make([]float64, len(x))
-	copy(y, x)
-	if len(ds) == 0 { // all standard normal variables
-		return
-	}
-	for i, typ := range ds {
-		switch typ {
-		case "nrm":
-			y[i] = (x[i] - μ[i]) / σ[i]
-		case "logMS", "logMuSig":
-			if x[i] < TOLMINLOG {
-				return nil, true
-			}
-			μN, σN := lognormal_calc_equiv_prms(μ[i], σ[i], x[i], typ == "logMS")
-			y[i] = (x[i] - μN) / σN
-		default:
-			chk.Panic("distribution %q is not available", typ)
-		}
-	}
-	return
-}
-
-// calc_orig_vars computes original variables from normal variables
-func calc_orig_vars(ds []string, μ, σ, y []float64) (x []float64, invalid bool) {
-	x = make([]float64, len(y))
-	copy(x, y)
-	if len(ds) == 0 { // all standard normal variables
-		return
-	}
-	for i, typ := range ds {
-		switch typ {
-		case "nrm":
-			x[i] = μ[i] + σ[i]*y[i]
-		case "logMS", "logMuSig":
-			if y[i] < TOLMINLOG {
-				return nil, true
-			}
-
-			// nonlinear problem for lognormal variable u[0] = x[i]
-			var nls num.NlSolver
-			nls.Init(1, func(fu, u []float64) error {
-				μNtmp, σNtmp := lognormal_calc_equiv_prms(μ[i], σ[i], u[0], typ == "logMS")
-				fu[0] = u[0] - (μNtmp + σNtmp*y[i])
-				return nil
-			}, nil, nil, false, true, nil)
-			u := []float64{μ[i] + σ[i]*y[i]}
-			nls.SetTols(1e-4, 1e-4, 1e-4, 1e-15)
-			nls.Lsearch = false
-			err := nls.Solve(u, true)
-			if err != nil {
-				chk.Panic("nonlinear solver failed:\n%v", err)
-			}
-			x[i] = u[0]
-
-		default:
-			chk.Panic("distribution %q is not available", typ)
-		}
-	}
-	return
-}
-
-// calc_beta calculates reliability index
-func calc_beta(best *goga.Individual, βref float64, ds []string, μ, σ, xref []float64, strategy int, verbose bool) (β float64) {
-	var xs, ys []float64
-	var invalid bool
-	if strategy < 3 {
-		xs = best.GetFloats()                      // check point
-		ys, invalid = calc_norm_vars(ds, μ, σ, xs) // standard normal variables
-	} else {
-		ys = best.GetFloats()                      // normal variables
-		xs, invalid = calc_orig_vars(ds, μ, σ, ys) // check point
-	}
-	if invalid {
-		chk.Panic("calc_beta: invalid value")
-	}
-	b := la.VecDot(ys, ys) // squared distance from origin in normalised space
-	β = math.Sqrt(b)
-	if verbose {
-		io.Pf("\nova  = %g  oor = %g\n", best.Ova, best.Oor)
-		io.Pf("x    = %v\n", xs)
-		io.Pf("xref = %v\n", xref)
-		io.PfYel("β    = %g", β)
-		io.Pf(" (%g)\n", βref)
-	}
-	return
 }
 
 // nice_num returns a truncated float
