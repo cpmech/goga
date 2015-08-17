7f225fa4fbd28f01e502e3ceda859bcad281450b
Dorival Pedroso <pedroso@cpmech.com>
2015-08-17 10:07:41 +1000

    preliminary documentation autogenerated by go doc added

---
+++ xgendoc.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+# Copyright 2015 Dorival Pedroso. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+import subprocess
+
+def Cmd(command, verbose=False, debug=False):
+    if debug:
+        print '=================================================='
+        print cmd
+        print '=================================================='
+    spr = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = spr.stdout.read()
+    err = spr.stderr.read().strip()
+    if verbose:
+        print out
+        print err
+    return out, err
+
+odir  = 'doc/'
+idxfn = odir+'index.html'
+licen = open('LICENSE', 'r').read()
+
+def header(title):
+    return """<html>
+<head>
+<meta http-equiv=\\"Content-Type\\" content=\\"text/html; charset=utf-8\\">
+<title>%s</title>
+<link type=\\"text/css\\" rel=\\"stylesheet\\" href=\\"static/style.css\\">
+<script type=\\"text/javascript\\" src=\\"static/godocs.js\\"></script>
+<style type=\\"text/css\\"></style>
+</head>
+<body>
+<div id=\\"page\\">""" % title
+
+def footer():
+    return """</div><!-- page -->
+<div id=\\"footer\\">
+<br /><br />
+<hr>
+<pre class=\\"copyright\\">
+%s</pre><!-- copyright -->
+</div><!-- footer -->
+</body>
+</html>""" % licen
+
+Cmd('echo "'+header('Goga &ndash; Documentation')+'" > '+idxfn)
+Cmd('echo "<h1>Goga &ndash; Documentation</h1>" >> '+idxfn)
+Cmd('echo "<h2 id=\\"pkg-index\\">Index</h2>\n<div id=\\"manual-nav\\">\n<dl>" >> '+idxfn)
+
+Cmd('godoc -html github.com/cpmech/goga >> '+idxfn)
+
+# fix links
+Cmd("sed -i -e 's@/src/target@https://github.com/cpmech/goga/blob/master/@g' "+idxfn+"")
+
+Cmd('echo "</dl>\n</div><!-- manual-nav -->" >> '+idxfn)
+Cmd('echo "'+footer()+'" >> '+idxfn)
