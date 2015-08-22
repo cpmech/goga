93ba372b2c2e959ff6d09c6f60224a5862699ac6
Dorival Pedroso <pedroso@cpmech.com>
2015-08-22 16:13:21 +1000

    post-proc function improved

--- definitions.go
+++ definitions.go
@@ -7,7 +7,7 @@ package goga
 import "bytes"
 
 // PostProc_t defines a function to post-process results
-type PostProc_t func(time int, pop Population)
+type PostProc_t func(idIsland, time int, pop Population)
 
 // Func_t defines a type for a generic function to be used as a gene value
 type Func_t func(ind *Individual) string
