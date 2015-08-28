c780d953101c0d964e639eb4d290b91dbff34d6c
Dorival Pedroso <pedroso@cpmech.com>
2015-08-28 23:06:44 +1000

    linewidth for g function in contour plot added

--- plotting.go
+++ plotting.go
@@ -26,6 +26,7 @@ type TwoVarsTrans_t func(x []float64) (y []float64, invalid bool)
 //   pop1    -- final population. can be <nil> if individuals are not to be plotted
 //   best    -- best individual. can be <nil>
 //   np      -- number of points for contour
+//   lw_g    -- linewidth for g functions
 //   extra   -- called just before saving figure
 //   axequal -- axis.equal
 //   vrange  -- [2][2] range of x and y values; e.g.: [][]float64{{xmin,xmax},{ymin,ymax}}
@@ -37,7 +38,7 @@ type TwoVarsTrans_t func(x []float64) (y []float64, invalid bool)
 //   f       -- function to plot filled contour. can be <nil>
 //   gs      -- functions to plot contour @ level 0. can be <nil>
 //  Note: g(x) operates on original x values
-func PlotTwoVarsContour(dirout, fnkey string, pop0, pop1 Population, best *Individual, np int, extra func(), axequal bool,
+func PlotTwoVarsContour(dirout, fnkey string, pop0, pop1 Population, best *Individual, np int, lw_g float64, extra func(), axequal bool,
 	vrange [][]float64, istrans, tplot bool, T, Ti TwoVarsTrans_t, f TwoVarsFunc_t, gs ...TwoVarsFunc_t) {
 	if fnkey == "" {
 		return
@@ -93,7 +94,7 @@ func PlotTwoVarsContour(dirout, fnkey string, pop0, pop1 Population, best *Indiv
 		plt.Contour(V0, V1, Zf, io.Sf("fsz=7, cmapidx=%d", cmapidx))
 	}
 	for k, _ := range gs {
-		plt.ContourSimple(V0, V1, Zg[k], "zorder=5, levels=[0], colors=['yellow'], linewidths=[2], clip_on=0")
+		plt.ContourSimple(V0, V1, Zg[k], io.Sf("zorder=5, levels=[0], colors=['yellow'], linewidths=[%g], clip_on=0", lw_g))
 	}
 	get_v := func(ind *Individual) (v []float64) {
 		v = ind.GetFloats()
