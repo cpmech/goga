69585d766356b81a4483ce11feda52e642eb6055
Dorival Pedroso <pedroso@cpmech.com>
2016-01-04 14:25:36 +1000

    tex report improved with multiple tables

--- report.go
+++ report.go
@@ -15,21 +15,26 @@ import (
 func TexDocumentStart() (buf *bytes.Buffer) {
 	buf = new(bytes.Buffer)
 	io.Ff(buf, `\documentclass[a4paper]{article}
+
 \usepackage{mydefaults}
 \usepackage[margin=1.5cm,footskip=0.5cm]{geometry}
+
 \title{GOGA Report}
 \author{Dorival Pedroso}
+
 \begin{document}
 `)
 	return
 }
 
 func TexDocumentEnd(buf *bytes.Buffer) {
-	io.Ff(buf, `\end{document}`)
+	io.Ff(buf, `
+\end{document}`)
 }
 
 func TexSingleObjTableStart(buf *bytes.Buffer, ntrials int) {
-	io.Ff(buf, `\begin{table} \centering
+	io.Ff(buf, `
+\begin{table} \centering
 \caption{Constrained single objective problems: Results}
 \begin{tabular}[c]{cccc} \toprule
 P & settings & results & histogram ($N_{trials}=%d$) \\ \hline
@@ -42,7 +47,7 @@ func TexSingleObjTableEnd(buf *bytes.Buffer) {
 \end{table}`)
 }
 
-func (o *Optimiser) TexSingleObjTableItem(buf *bytes.Buffer, problem int, fref float64, fmtFave, fmtFdev, fmtHist, fmtX string) {
+func TexSingleObjTableItem(o *Optimiser, buf *bytes.Buffer, problem int, fref float64, fmtFave, fmtFdev, fmtHist, fmtX string) {
 	hlen := 28
 	SortByOva(o.Solutions, 0)
 	best := o.Solutions[0]
@@ -76,16 +81,48 @@ func (o *Optimiser) TexSingleObjTableItem(buf *bytes.Buffer, problem int, fref f
 		best.Flt)
 }
 
-func TexWrite(fnkey string, buf *bytes.Buffer, dorun bool) {
+func TexWrite(dirout, fnkey string, buf *bytes.Buffer, dorun bool) {
 	tex := fnkey + ".tex"
-	io.WriteFileVD("/tmp/goga", tex, buf)
+	io.WriteFileVD(dirout, tex, buf)
 	if dorun {
 		_, err := io.RunCmd(true, "pdflatex", "-interaction=batchmode", "-halt-on-error", "-output-directory=/tmp/goga/", tex)
 		if err != nil {
 			chk.Panic("%v", err)
 		}
-		io.PfBlue("file </tmp/goga/%s.pdf> generated\n", fnkey)
+		io.PfBlue("file <%s/%s.pdf> generated\n", dirout, fnkey)
 	}
+}
+
+// TexReport produces TeX report
+//  nRowPerTab -- number of rows per table
+func TexReport(dirout, fnkey string, ntrials, nRowPerTab int, opts []*Optimiser, frefs []float64, fmtHist []string, dorun bool) {
+	nprob := len(opts)
+	if nRowPerTab < 1 {
+		chk.Panic("number of rows per table must be greater than 0")
+	}
+	chk.IntAssert(len(frefs), nprob)
+	chk.IntAssert(len(fmtHist), nprob)
+	buf := TexDocumentStart()
+	for i, opt := range opts {
+		if i%nRowPerTab == 0 {
+			if i > 0 {
+				io.Ff(buf, `\bottomrule`)
+				TexSingleObjTableEnd(buf) // end previous table
+				io.Ff(buf, "\n")
+			}
+			TexSingleObjTableStart(buf, ntrials) // begin new table
+		} else {
+			if i > 0 {
+				io.Ff(buf, `\hline`)
+			}
+		}
+		TexSingleObjTableItem(opt, buf, i+1, frefs[i], "%.8f", "%.9f", fmtHist[i], "%.2f")
+	}
+	io.Ff(buf, `\bottomrule`)
+	TexSingleObjTableEnd(buf) // end previous table
+	io.Ff(buf, "\n")
+	TexDocumentEnd(buf)
+	TexWrite(dirout, fnkey, buf, dorun)
 }
 
 // nice_num returns a truncated float
