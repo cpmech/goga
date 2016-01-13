4966411c0de518c4c78498f741dd1f1bf7682c32
Dorival Pedroso <pedroso@cpmech.com>
2016-01-13 11:58:14 +1000

    important change: best compromise for single-obj functions and multi-obj functions implemented: Cmax coefficient has to decrease with number of variables

--- operators.go
+++ operators.go
@@ -4,27 +4,28 @@
 
 package goga
 
-import "github.com/cpmech/gosl/rnd"
+import (
+	"math"
+
+	"github.com/cpmech/gosl/rnd"
+)
 
 // DiffEvol performs the differential-evolution operation
 func DiffEvol(xnew, x, x0, x1, x2 []float64, prms *Parameters) {
-	C := rnd.Float64(0.0, 1.0)
-	F := rnd.Float64(0.0, 1.0)
-	K := 0.5 * (F + 1.0)
 	n := len(x)
+	C := rnd.Float64(0.0, 1.0/math.Pow(float64(n), 0.1))
+	F := rnd.Float64(0.0, 1.0)
 	I := rnd.Int(0, n-1)
-	mutation := rnd.FlipCoin(0.5)
 	for i := 0; i < n; i++ {
 		if rnd.FlipCoin(C) || i == I {
-			if mutation {
-				xnew[i] = x0[i] + K*(x1[i]+x2[i]-2.0*x0[i])
-			} else {
-				xnew[i] = x0[i] + F*(x1[i]-x2[i])
+			xnew[i] = x0[i] + F*(x1[i]-x2[i])
+			if xnew[i] < prms.FltMin[i] {
+				xnew[i] = prms.FltMin[i]
+			}
+			if xnew[i] > prms.FltMax[i] {
+				xnew[i] = prms.FltMax[i]
 			}
 		} else {
-			xnew[i] = x[i]
-		}
-		if xnew[i] < prms.FltMin[i] || xnew[i] > prms.FltMax[i] {
 			xnew[i] = x[i]
 		}
 	}
