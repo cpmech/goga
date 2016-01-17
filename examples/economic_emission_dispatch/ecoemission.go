77c51014ff07770725c408f299b0abe0b9707b92
Dorival Pedroso <pedroso@cpmech.com>
2016-01-17 17:31:31 +1000

    example added: economic emission dispatch problem

---
+++ examples/economic_emission_dispatch/ecoemission.go
@@ -0,0 +1,198 @@
+// Copyright 2012 Dorival de Moraes Pedroso. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"math"
+
+	"github.com/cpmech/goga"
+	"github.com/cpmech/gosl/io"
+	"github.com/cpmech/gosl/la"
+	"github.com/cpmech/gosl/plt"
+)
+
+func main() {
+
+	// goga parameters
+	opt := new(goga.Optimiser)
+	opt.Default()
+	opt.Nsol = 120
+	opt.Ncpu = 4
+	opt.Tf = 500
+	opt.Verbose = false
+	opt.Ntrials = 1
+	opt.GenType = "latin"
+	opt.EpsH = 1e-4
+
+	// flags
+	problem := 3 // 1=cost only; 2=emission only; default=cost and emission
+	checkOnly := false
+
+	// generators
+	Pdemand := 2.834
+	Ploss := 0.0
+	gs := NewGenerators(false)
+	ngs := len(gs) // number of generators == number of variables (Nx)
+	opt.FltMin = make([]float64, ngs)
+	opt.FltMax = make([]float64, ngs)
+	for i := 0; i < ngs; i++ {
+		opt.FltMin[i], opt.FltMax[i] = gs[i].Pmin, gs[i].Pmax
+	}
+
+	// problem variables
+	var nf, ng, nh int // number of objective functions and constraints
+	var fcn goga.MinProb_t
+
+	// problems
+	switch problem {
+
+	// problem # 1: lossless and unsecured: cost only
+	case 1:
+		opt.RptXref = []float64{0.10954, 0.29967, 0.52447, 1.01601, 0.52469, 0.35963}
+		opt.RptFref = []float64{600.114}
+		nf, nh = 1, 1
+		fcn = func(f, g, h, x []float64, ξ []int, cpu int) {
+			sumP := la.VecAccum(x)
+			f[0] = gs.FuelCost(x)
+			h[0] = sumP - Pdemand - Ploss
+		}
+
+	// lossless and unsecured: emission only
+	case 2:
+		opt.RptXref = []float64{0.40584, 0.45915, 0.53797, 0.38300, 0.53791, 0.51012}
+		opt.RptFref = []float64{0.19420}
+		nf, nh = 1, 1
+		fcn = func(f, g, h, x []float64, ξ []int, cpu int) {
+			sumP := la.VecAccum(x)
+			f[0] = gs.Emission(x)
+			h[0] = sumP - Pdemand - Ploss
+		}
+
+	// lossless and unsecured: cost and emission
+	default:
+		nf, nh = 2, 1
+		fcn = func(f, g, h, x []float64, ξ []int, cpu int) {
+			sumP := la.VecAccum(x)
+			f[0] = gs.FuelCost(x)
+			f[1] = gs.Emission(x)
+			h[0] = sumP - Pdemand - Ploss
+		}
+	}
+
+	// check best known solution
+	if checkOnly {
+		check(fcn, ng, nh, opt.RptXref, opt.RptFref[0], 1e-6)
+		return
+	}
+
+	// initialise optimiser
+	opt.Init(goga.GenTrialSolutions, nil, fcn, nf, ng, nh)
+
+	// initial solutions
+	var sols0 []*goga.Solution
+	if false {
+		sols0 = opt.GetSolutionsCopy()
+	}
+
+	// solve
+	opt.RunMany("", "")
+
+	// post-processing
+	switch problem {
+	case 1:
+
+		// results
+		print_single(opt, gs, opt.RptXref, opt.RptFref[0], 0.22214, Pdemand, Ploss)
+
+		// stat
+		io.Pf("\n")
+		goga.StatF(opt, 0, true)
+
+	case 2:
+
+		// results
+		print_single(opt, gs, opt.RptXref, 638.260, opt.RptFref[0], Pdemand, Ploss)
+
+		// stat
+		io.Pf("\n")
+		goga.StatF(opt, 0, true)
+
+	default:
+
+		// stat
+		goga.StatF1F0(opt, true)
+
+		// plot
+		feasibleOnly := true
+		plt.SetForEps(0.8, 300)
+		fmtAll := &plt.Fmt{L: "final solutions", M: ".", C: "orange", Ls: "none", Ms: 3}
+		fmtFront := &plt.Fmt{L: "final Pareto front", C: "r", M: "o", Ms: 3, Ls: "none"}
+		goga.PlotOvaOvaPareto(opt, sols0, 0, 1, feasibleOnly, fmtAll, fmtFront)
+		_, dat, _ := io.ReadTable("abido-fig6.dat")
+		plt.Plot(dat["cost"], dat["emission"], "'b-', label='reference'")
+		plt.Gll("$f_0:\\;$ cost~[\\$/h]", "$f_1:\\quad$ emission~[ton/h]", "")
+		plt.SaveD("/tmp/goga", "ecoemission.eps")
+
+		// report
+		goga.TexF1F0Report("/tmp/goga", "tmp_ecoemission", "ecoemission", 10, true, []*goga.Optimiser{opt})
+		goga.TexF1F0Report("/tmp/goga", "ecoemission", "ecoemission", 10, false, []*goga.Optimiser{opt})
+	}
+}
+
+func print_single(opt *goga.Optimiser, gs Generators, Pref []float64, costRef, emisRef, Pdemand, Ploss float64) {
+	n := 9*2 + 8*6 + 12
+	P := Pref
+	sumP := la.VecAccum(P)
+	bal_err := math.Abs(sumP - Pdemand - Ploss)
+	io.Pf("%s", io.StrThickLine(n))
+	io.Pf("%9s%9s%8s%8s%8s%8s%8s%8s%12s\n", "cost", "emis", "P1", "P2", "P3", "P4", "P5", "P6", "bal.err")
+	io.Pf("%s", io.StrThinLine(n))
+	io.Pfyel("%9.4f%9.5f%8.5f%8.5f%8.5f%8.5f%8.5f%8.5f%12.4e\n", costRef, emisRef, P[0], P[1], P[2], P[3], P[4], P[5], bal_err)
+	goga.SortByOva(opt.Solutions, 0)
+	for i, sol := range opt.Solutions {
+		P = sol.Flt
+		sumP = la.VecAccum(P)
+		cost := gs.FuelCost(P)
+		emis := gs.Emission(P)
+		bal_err = math.Abs(sumP - Pdemand - Ploss)
+		io.Pf("%9.4f%9.5f%8.5f%8.5f%8.5f%8.5f%8.5f%8.5f%12.4e\n", cost, emis, P[0], P[1], P[2], P[3], P[4], P[5], bal_err)
+		if i > 20 {
+			break
+		}
+	}
+	io.Pf("%s", io.StrThickLine(n))
+}
+
+func check(fcn goga.MinProb_t, ng, nh int, xs []float64, fs, ϵ float64) {
+	f := make([]float64, 1)
+	g := make([]float64, ng)
+	h := make([]float64, nh)
+	cpu := 0
+	fcn(f, g, h, xs, nil, cpu)
+	io.Pfblue2("xs = %v\n", xs)
+	io.Pfblue2("f(x)=%g  (%g)  diff=%g\n", f[0], fs, math.Abs(fs-f[0]))
+	for i, v := range g {
+		unfeasible := false
+		if v < 0 {
+			unfeasible = true
+		}
+		if unfeasible {
+			io.Pfred("g%d(x) = %g\n", i, v)
+		} else {
+			io.Pfgreen("g%d(x) = %g\n", i, v)
+		}
+	}
+	for i, v := range h {
+		unfeasible := false
+		if math.Abs(v) > ϵ {
+			unfeasible = true
+		}
+		if unfeasible {
+			io.Pfred("h%d(x) = %g\n", i, v)
+		} else {
+			io.Pfgreen("h%d(x) = %g\n", i, v)
+		}
+	}
+}
---
+++ examples/economic_emission_dispatch/generators.go
@@ -0,0 +1,116 @@
+// Copyright 2012 Dorival de Moraes Pedroso. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"math"
+
+	"github.com/cpmech/gosl/io"
+	"github.com/cpmech/gosl/utl"
+)
+
+// Generator holds data for one generator
+type Generator struct {
+	a, b, c       float64 // cost coefficients
+	α, β, γ, ζ, λ float64 // emission coefficients
+	Pmin, Pmax    float64 // capacity constraints
+}
+
+// Generators holds all generators
+type Generators []Generator
+
+// FuelCost computes the total $/h fuel cost for given P[i] (power outputs)
+func (o Generators) FuelCost(P []float64) (C float64) {
+	for i, g := range o {
+		C += g.a + g.b*P[i] + g.c*P[i]*P[i]
+	}
+	return
+}
+
+// Emission computes the total ton/h emmision of atmospheric pollutants
+func (o Generators) Emission(P []float64) (E float64) {
+	for i, g := range o {
+		E += 0.01*(g.α+g.β*P[i]+g.γ*P[i]*P[i]) + g.ζ*math.Exp(g.λ*P[i])
+	}
+	return
+}
+
+// PrintConstraints prints violated or not constraints
+func (o Generators) PrintConstraints(P []float64, Pdemand float64, full bool) {
+	sumP := 0.0
+	for i, g := range o {
+		if full {
+			io.Pfyel("P%d range error = %v\n", i, utl.GtePenalty(P[i], g.Pmin, 1)+utl.GtePenalty(g.Pmax, P[i], 1))
+		}
+		sumP += P[i]
+	}
+	Ploss := 0.0
+	io.Pf("balance error = %v\n", math.Abs(sumP-Pdemand-Ploss))
+}
+
+// NewGenrators loads generators
+func NewGenerators(check bool) (gs Generators) {
+
+	// my best: lossless and unsecured: cost only
+	// 599.9676 0.09624 0.31397 0.51988 1.02003 0.50812 0.37476  9.9986e-04
+
+	// units:
+	//  a [$ / (hMW²)]
+	//  b [$ / (hMW)]
+	//  c [$ / h]
+	//  α [tons / (hMW²)]
+	//  β [tons / (hMW)]
+	//  γ [tons / h]
+	//  ζ [tons / h]
+	//  λ [MW⁻¹]
+	//  Pmin [MW / 100]
+	//  Pmax [MW / 100]
+
+	gs = Generators{
+		{a: 10, b: 200, c: 100, α: 4.091, β: -5.554, γ: 6.490, ζ: 2.0e-4, λ: 2.857, Pmin: 0.05, Pmax: 0.5},
+		{a: 10, b: 150, c: 120, α: 2.543, β: -6.047, γ: 5.638, ζ: 5.0e-4, λ: 3.333, Pmin: 0.05, Pmax: 0.6},
+		{a: 20, b: 180, c: 40., α: 4.258, β: -5.094, γ: 4.586, ζ: 1.0e-6, λ: 8.000, Pmin: 0.05, Pmax: 1.0},
+		{a: 10, b: 100, c: 60., α: 5.326, β: -3.550, γ: 3.380, ζ: 2.0e-3, λ: 2.000, Pmin: 0.05, Pmax: 1.2},
+		{a: 20, b: 180, c: 40., α: 4.258, β: -5.094, γ: 4.586, ζ: 1.0e-6, λ: 8.000, Pmin: 0.05, Pmax: 1.0},
+		{a: 10, b: 150, c: 100, α: 6.131, β: -5.555, γ: 5.151, ζ: 1.0e-5, λ: 6.667, Pmin: 0.05, Pmax: 0.6},
+	}
+
+	if check {
+
+		// lossless and unsecured: cost only
+		P_best_cost := []float64{0.10954, 0.29967, 0.52447, 1.01601, 0.52469, 0.35963}
+		c := gs.FuelCost(P_best_cost)
+		e := gs.Emission(P_best_cost)
+		io.Pf("lossless and unsecured: cost only\n")
+		io.Pforan("c = %.3f (600.114)\n", c)
+		io.Pforan("e = %.5f (0.22214)\n", e)
+		P_best_cost = []float64{0.1265, 0.2843, 0.5643, 1.0468, 0.5278, 0.2801}
+		c = gs.FuelCost(P_best_cost)
+		io.Pfgreen("c = %.3f\n", c)
+		Pdemand := 2.834
+		gs.PrintConstraints(P_best_cost, Pdemand, true)
+
+		// lossless and unsecured: emission only
+		P_best_emission := []float64{0.40584, 0.45915, 0.53797, 0.38300, 0.53791, 0.51012}
+		c = gs.FuelCost(P_best_emission)
+		e = gs.Emission(P_best_emission)
+		io.Pf("\nlossless and unsecured: emission only\n")
+		io.Pforan("c = %.3f (638.260)\n", c)
+		io.Pforan("e = %.5f (0.19420)\n", e)
+
+		P_best_cost = []float64{0.1500, 0.3000, 0.5500, 1.0500, 0.4600, 0.3500}
+		c = gs.FuelCost(P_best_cost)
+		e = gs.Emission(P_best_cost)
+		io.Pforan("\nc = %.3f (606.314)\n", c)
+		io.Pforan("e = %.5f (0.22330)\n", e)
+
+		P_best_emission = []float64{0.4000, 0.4500, 0.5500, 0.4000, 0.5500, 0.5000}
+		c = gs.FuelCost(P_best_emission)
+		e = gs.Emission(P_best_emission)
+		io.Pforan("\nc = %.3f (639.600)\n", c)
+		io.Pforan("e = %.5f (0.19424)\n", e)
+	}
+	return
+}
